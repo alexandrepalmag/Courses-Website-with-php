160d1de42d7837f45bfdd995e47ee66edd317e0e
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-08-29 12:11:11 -0300

    creation of the initial project files

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>School of Courses</title>
+</head>
+<body>
+    <header src="" title="" alt="">
+
+    </header>
+    <div id="content">
+   
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//database
+include 'db.php';
+//page header
+include 'header.php';
+//page content
+include 'home.php';
+//page footer
+include 'footer.php';
