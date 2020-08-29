4d70851b67c2f994e04c5d25cba108fff12dbace
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-08-29 13:50:15 -0300

    Layout creation: home

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
 </div>
 </body>
-<footer>
-    
+<footer class="footer mt-auto py-3">
+    <div class="container">
+        <span class="text-muted">School of Courses 2020</span>
+    </div>
 </footer>
+<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+
 </html>
--- header.php
+++ header.php
@@ -1,13 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>School of Courses</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
 </head>
+
 <body>
     <header src="" title="" alt="">
-
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" id="navGeneral">
+            <div class="container">
+            <a class="navbar-brand" href="#">School of Courses</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
     </header>
-    <div id="content">
-   
+    <div class="container" id="content">
