d045764159b03dbc29fb2d12ed5c79387907b0f0
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-08-31 23:39:58 -0300

    delete query to database

--- deletecourse.php
+++ deletecourse.php
@@ -1,3 +1,11 @@
 <?php
 
-echo $_GET['id_course'];
+include 'db.php';
+
+$id_course = $_GET['id_course'];
+
+$query = "DELETE FROM COURSES WHERE id_course = $id_course";
+
+mysqli_query($conn, $query);
+
+header('location:index.php?page=courses');
